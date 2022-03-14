ab1165f5e0959227c0b1abaa498d937ff61c94a7
Omkar Nale <omkarnale940@gmail.com>
2022-03-14 20:26:57 +0530

    Lab session - 1 email application

---
+++ EmailApplication_Lab_Session_1/src/com/greatlearning/labsession/App/Driver.java
@@ -0,0 +1,13 @@
+package com.greatlearning.labsession.App;
+
+import com.greatlearning.labsession.UI.EmailApplicationUI;
+
+public class Driver {
+
+public static void main(String[] args) {
+		
+		EmailApplicationUI ui = new EmailApplicationUI();
+		
+		ui.init();
+	}
+}
