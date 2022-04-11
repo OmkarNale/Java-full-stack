a206e465afd31c091db1903c936edbee98ab83da
Omkar Nale <omkarnale940@gmail.com>
2022-04-11 20:36:43 +0530

    second lab session on data struture 10-04-2022

---
+++ Java-lab-session-2/src/com/firstproblem/paymoney/driver/Main.java
@@ -0,0 +1,13 @@
+package com.firstproblem.paymoney.driver;
+
+import com.firstproblem.paymoney.service.PayMoneyTransactionUI;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		
+		PayMoneyTransactionUI ui = new PayMoneyTransactionUI();
+		ui.init();		
+	}
+}
---
+++ Java-lab-session-2/src/com/secondproblem/currencydenominations/driver/Main.java
@@ -0,0 +1,12 @@
+package com.secondproblem.currencydenominations.driver;
+
+import com.secondproblem.currencydenominations.services.TravellerCurrencySpendUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TravellerCurrencySpendUI ui = new TravellerCurrencySpendUI();
+		ui.init();
+	}
+}
