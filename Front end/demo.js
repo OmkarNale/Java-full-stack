3029800b30f04ab0d8e00451f748a6d671d596eb
Omkar Nale <omkarnale940@gmail.com>
2022-09-26 22:24:46 +0530

    Lab 3 - Front End - Advanced JavaScript

---
+++ Front end/demo.js
@@ -0,0 +1,15 @@
+var button=document.getElementById("button");
+
+button.onclick=function(){
+
+console.log("Button clicked");
+
+}
+
+ 
+
+button.onclick=function(){
+
+console.log("Button clicked updated");
+
+}
